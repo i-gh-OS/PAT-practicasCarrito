b19c31c58e899200ee63e71f9d8aa29272088ace
i-gh-OS <202301839@alu.comillas.edu>
2026-03-05 15:59:42 +0100

    Create CarritoService.java

---
+++ practicasCarrito/src/main/java/edu/comillas/icai/gitt/pat/spring/mvc/service/CarritoService.java
@@ -0,0 +1,30 @@
+package edu.comillas.icai.gitt.pat.spring.mvc.service;
+
+import edu.comillas.icai.gitt.pat.spring.mvc.modelo.Carrito;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CarritoService {
+
+    public List<Carrito> getCarritos() {
+        throw new UnsupportedOperationException("Pendiente de implementar con JPA");
+    }
+
+    public Carrito crearCarrito(Carrito carrito) {
+        throw new UnsupportedOperationException("Pendiente de implementar con JPA");
+    }
+
+    public Carrito getCarrito(long idCarrito) {
+        throw new UnsupportedOperationException("Pendiente de implementar con JPA");
+    }
+
+    public void borrarCarrito(long idCarrito) {
+        throw new UnsupportedOperationException("Pendiente de implementar con JPA");
+    }
+
+    public Carrito modificarCarrito(long idCarrito, Carrito carrito) {
+        throw new UnsupportedOperationException("Pendiente de implementar con JPA");
+    }
+}
