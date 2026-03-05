67d28dc600c597346e9a0565f816e25e730af329
i-gh-OS <202301839@alu.comillas.edu>
2026-03-05 16:06:51 +0100

    cambiado a extends CRUD repository no JPA

--- practicasCarrito/src/main/java/edu/comillas/icai/gitt/pat/spring/mvc/repository/CarritoRepository.java
+++ practicasCarrito/src/main/java/edu/comillas/icai/gitt/pat/spring/mvc/repository/CarritoRepository.java
@@ -2,6 +2,6 @@ package edu.comillas.icai.gitt.pat.spring.mvc.repository;
 
 import edu.comillas.icai.gitt.pat.spring.mvc.modelo.Carrito;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
-public interface CarritoRepository extends JpaRepository<Carrito, Long> {
-}
+public interface CarritoRepository extends CrudRepository<Carrito, Long> {}
--- practicasCarrito/src/main/java/edu/comillas/icai/gitt/pat/spring/mvc/repository/LineaCarritoRepository.java
+++ practicasCarrito/src/main/java/edu/comillas/icai/gitt/pat/spring/mvc/repository/LineaCarritoRepository.java
@@ -2,12 +2,11 @@ package edu.comillas.icai.gitt.pat.spring.mvc.repository;
 
 import edu.comillas.icai.gitt.pat.spring.mvc.modelo.LineaCarrito;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
 
-public interface LineaCarritoRepository extends JpaRepository<LineaCarrito, Long> {
-
+public interface LineaCarritoRepository extends CrudRepository<LineaCarrito, Long> {
     Optional<LineaCarrito> findByCarrito_IdCarritoAndIdArticulo(Long idCarrito, Long idArticulo);
-
     Optional<LineaCarrito> findByIdLineaAndCarrito_IdCarrito(Long idLinea, Long idCarrito);
 }
