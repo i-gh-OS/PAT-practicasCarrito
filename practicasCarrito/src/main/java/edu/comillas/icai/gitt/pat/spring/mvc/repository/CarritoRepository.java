61249505f90760551d7a1b8dfa34d44c1fa590ac
i-gh-OS <202301839@alu.comillas.edu>
2026-04-25 20:24:38 +0200

    act

--- practicasCarrito/src/main/java/edu/comillas/icai/gitt/pat/spring/mvc/repository/CarritoRepository.java
+++
@@ -1,7 +0,0 @@
-package edu.comillas.icai.gitt.pat.spring.mvc.repository;
-
-import edu.comillas.icai.gitt.pat.spring.mvc.modelo.Carrito;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CarritoRepository extends CrudRepository<Carrito, Long> {}
